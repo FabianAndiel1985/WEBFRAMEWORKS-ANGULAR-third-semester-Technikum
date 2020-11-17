b7b0c75457c457b6248f7b333f45011bab1df337
Fabian Andiel <fabian.andiel@yahoo.com>
2020-11-17 17:11:08 +0100

    adds add-user-component add-user-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { EnterFormComponent } from './add-user/enter-form/enter-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddUserComponent,
+    EnterFormComponent
   ],
   imports: [
     BrowserModule,
